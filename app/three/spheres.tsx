2336f77081bfd1c7ef647c5ea2b9d809c6b010ff
tiberiuioan35@gmail.com <tiberiuioan35@gmail.com>
2024-12-10 16:29:33 +0200

    Initial commit

---
+++ app/three/spheres.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
+import { Mesh } from 'three';
+
+const Sphere: React.FC<any> = (props) => {
+  const meshRef = useRef<Mesh>(null);
+
+  return (
+    <mesh {...props} ref={meshRef}>
+      <sphereGeometry args={[1, 64, 32]} />
+      <meshStandardMaterial color="#1A78C3" />
+    </mesh>
+  );
+};
+
+const Background = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className="background">
+      <Canvas
+        camera={{
+          position: [0, 0, 10],
+          fov: 50,
+        }}
+      >
+        <ambientLight intensity={1.2} />
+        <directionalLight position={[-5, 5, 10]} intensity={2} />
+        <Spheres scrollY={scrollY} />
+      </Canvas>
+    </div>
+  );
+};
+
+const Spheres: React.FC<{ scrollY: number }> = ({ scrollY }) => {
+  const groupRef = useRef<Mesh>(null);
+
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.position.y = scrollY * 0.01;
+    }
+  });
+
+  return (
+    <group ref={groupRef}>
+      <Sphere position={[4.5, 4.5, 0]} scale={1.5} />
+      <Sphere position={[1, -1, 0]} scale={2} />
+      <Sphere position={[5, -8, 0]} scale={3} />
+    </group>
+  );
+};
+
+export default Background;
