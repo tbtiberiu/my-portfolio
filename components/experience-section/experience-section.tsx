a57b192e096b7412df881b680e6a7818d35923aa
tiberiuioan35@gmail.com <tiberiuioan35@gmail.com>
2024-12-29 18:25:22 +0200

    Add better experience descriptions

--- components/experience-section/experience-section.tsx
+++ components/experience-section/experience-section.tsx
@@ -9,7 +9,7 @@ const experiences: ExperienceInfo[] = [
     location: 'Brasov, Romania (Remote)',
     dates: 'December 2023 - Present',
     description:
-      'Developed and updated workflows for Angular projects such as the Alliance iS HPLC System Kiosk, Console, and Method Editor apps. I contributed by building new features and improving existing ones, collaborating closely with designers and backend developers to ensure a seamless user experience. Additionally, I fixed bugs, optimized application performance, and participated in team meetings, code reviews, and planning sessions to keep projects aligned with goals and deliver high-quality solutions.',
+      'Developed new user stories, fix bugs, create and review pull requests in multiple Angular projects such as the Alliance iS HPLC System Kiosk, Console, and Method Editor apps to meet specific team goals.',
     technologies: ['HTML', 'CSS', 'Typescript', 'Angular', 'Git'],
   },
   {
@@ -18,13 +18,12 @@ const experiences: ExperienceInfo[] = [
     location: 'Brasov, Romania (Remote)',
     dates: 'December 2022 - December 2023',
     description:
-      'Gained strong skills in crafting and maintaining unit tests and BDD scenarios to ensure software quality and functionality. I developed BDD tests, fixed bugs, and refactored a critical part of the logging mechanism in a Python-based internal testing tool. Throughout, I documented my work clearly through code comments, README files, and Confluence, while effectively communicating progress to the team.',
+      'Gained strong skills in crafting and maintaining unit tests and BDD scenarios to ensure software quality and functionality. I developed BDD tests, fixed bugs, and refactored a critical part of the logging mechanism in a Python-based internal testing tool.',
     technologies: [
       'Python',
       'Pytest BDD',
       'Git',
       'Selenium WebDriver',
-      'SpecFlow',
       'Jenkins',
     ],
   },
@@ -34,7 +33,7 @@ const experiences: ExperienceInfo[] = [
     location: 'Brasov, Romania (Hybrid)',
     dates: 'August 2022 - November 2022',
     description:
-      "After completing a two month training program in Javascript, React, ASP.NET, and Azure Cloud, I successfully completed all assigned tasks involving these technologies. Building on this foundation, I played a key role as the main DevOps engineer in the development of a Recipe Keeper app, where I managed the deployment of frontend and backend components and set up CI/CD pipelines to ensure smooth and efficient delivery. This project allowed me to apply and expand my skills in cloud services and full-stack development, contributing significantly to the app's success.",
+      'Acquired a solid foundation in React app development, ASP.NET API creation, Azure Cloud hosting, and Agile methodologies. Successfully executed tasks utilizing HTML, CSS, JavaScript, React, and Azure.',
     technologies: [
       'Javascript',
       'React',
